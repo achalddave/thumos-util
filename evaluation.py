8890382fc3caaea14c4ad101263d43184d4b3654
Achal Dave <achalddave@live.com>
2016-03-06 02:05:43 -0500

    Move binarized_predictions_to_detections to util/evaluation

--- evaluation.py
+++ evaluation.py
@@ -2,9 +2,44 @@ import collections
 import subprocess
 import os
 
+import numpy as np
+
 Detection = collections.namedtuple('Detection',
                                    ['filename', 'start_seconds', 'end_seconds',
                                     'category', 'score'])
+
+
+# TODO(achald): Is this the best place for this function?
+def binarized_predictions_to_detection_tuples(binary_predictions):
+    """
+    Args:
+        binary_predictions ((num_frames, 1) array): Binarized predictions for
+            each frame in a video.
+
+    Returns:
+        detections (list of (start, end) tuples): Note that this is an
+            un-Pythonic range, in that the end is part of the detection.
+
+    >>> predictions = np.array([0, 0, 0, 1, 1, 1, 0, 0, 1])
+    >>> predictions = predictions.reshape((len(predictions), 1))
+    >>> binarized_predictions_to_detections(predictions)
+    [(3, 5), (8, 8)]
+    """
+    # Pad with 0s on both sides to detect changes at the beginning and end
+    padded_binary_predictions = np.vstack(([0], binary_predictions, [0]))
+    # state_changes[i] = +1 => padded_binary_predictions[i] = 0,
+    #                          padded_binary_predictions[i+1] = 1
+    #                       => binary_predictions[i-1] = 0,
+    #                          binary_predictions[i] = 1
+    # state_changes[i] = -1 => padded_binary_predictions[i] = 1,
+    #                          padded_binary_predictions[i+1] = 0
+    #                       => binary_predictions[i-1] = 1
+    #                          binary_predictions[i] = 0
+    state_changes = np.diff(padded_binary_predictions, axis=0)
+    starts = np.where(state_changes == 1)[0]
+    ends = np.where(state_changes == -1)[0] - 1  # Include end in detection
+    return [(start, end) for start, end in zip(starts, ends)]
+
 
 def dump_detections(detections, output_path):
     """
@@ -15,6 +50,7 @@ def dump_detections(detections, output_path):
         for detection in detections:
             f.write(('{filename} {start_seconds} {end_seconds} '
                      '{category} {score}\n').format(**detection._asdict()))
+
 
 def evaluate_detections(detections,
                         detections_output_path,
